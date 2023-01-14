6d12bde32febb5cd5b812150e47976b84a82b8b0
Frank Cairampoma <ingfralch@gmail.com>
2023-01-14 11:38:07 -0500

    Ordenando y explicando el codigo

--- src/sockets.js
+++ src/sockets.js
@@ -1,36 +1,53 @@
 module.exports = function (io) {
+    // Crea un arreglo para almacenar los apodos de usuario
     let nicknames = [];
     
+    // Establece un manejador de eventos para la conexión de socket
     io.on("connection", function (socket) {
         console.log("Nuevo usuario conectado");
-        // Escuchando el evento chatMessage
-        socket.on("chatMessage", function (msg) {
-            // console.log(msg);
-            // io.sockets.emit("chatMessage", msg);
-            io.emit("chatMessage", {
-                 msg,
-                nick: socket.nickname
-            });
-        });
-
-        // Escuchando el evento newUser
+       
+        // Escucha el evento "newUser"
         socket.on("newUser", function (nick, callback) {
+            // Verifica si el apodo de usuario ya existe en la lista de apodos
             if (nicknames.indexOf(nick) != -1) {
+                // Si existe, llama al callback con un valor falso
                 callback(false);
             } else {
+                // Si no existe, llama al callback con un valor verdadero
                 callback(true);
-                socket.nickname = nick;
+                // Asigna el apodo de usuario al socket
+                socket.nickname = nick; 
+                /* ⬆️ para entender como se guarda la sesión del usuario 
+                   el codigo se ejecuta cada vez que se conecta un usuario
+                    y se guarda en el objeto socket el apodo del usuario
+                    y tambien se guarda en el arreglo nicknames, en otras palabras
+                    no se ejecuta una sola vez, sino cada vez que se conecta un usuario               
+                 */
+                // Agrega el apodo de usuario a la lista de apodos
                 nicknames.push(socket.nickname);
                 
+                // Emite un evento "NameNewusers" al cliente con la lista actualizada de apodos de usuarios
                 io.emit("NameNewusers", nicknames);
             }
         });
 
-        // Escuchando el evento disconnect
+        // Establece un manejador de eventos para el evento de desconexión del socket
         socket.on("disconnect", function (data) {
+            // Verifica si el socket tiene un apodo de usuario asignado
             if (!socket.nickname) return;
+            // Elimina el apodo de usuario de la lista de apodos
             nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+            // Emite un evento "NameNewusers" al cliente con la lista actualizada de apodos de usuarios
             io.emit("NameNewusers", nicknames);
+        });
+
+        // Establece un manejador de eventos para el evento "chatMessage"
+         socket.on("chatMessage", function (msg) {
+            // Emite un evento "chatMessage" al cliente con el mensaje y el apodo de usuario del socket
+            io.emit("chatMessage", {
+                msg,
+                nick: socket.nickname
+            });
         });
     });
 };
